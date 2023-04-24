2b338e2d1a5da64b69493991344ab41d5f60befb
mft14 <metalfort14@gmail.com>
2023-04-24 13:28:12 +0200

    Added Login

--- login.php
+++ login.php
@@ -6,8 +6,29 @@
 <link rel="stylesheet" type="text/css" href="src/main.css">
 
 </head>
-<body>
+<body class="bluebackground center">
+    <h1 class="fontwhite">Login</h1>
 
+    <div class="loginbox">
 
+        <form action="login.php" method="post">
+
+            <label for="username">Benutzername:</label>
+            <input type="text" id="username" name="username" class="textfield" required><br><br>
+
+            <label for="password">Passwort:</label>
+            <input type="password" id="password" name="password" class="textfield" required><br><br>
+
+            <label for="destination">Abteilung:</label>
+            <select id="destination" name="destination" class="textfield" style="height: 60px;">
+                <option value="page1.php" id="abtleiter">Abteilungsleiter</option>
+                <option value="page2.php" id="itabt">IT Abteilung</option>
+                <option value="page3.php" id="itabt2">IT Abteilung (Einkauf/Marketing)</option>
+                <option value="page4.php" id="personal">Personalabteilung</option>
+            </select><br><br>
+            <button type="submit" class="loginbutton">Anmelden</button>
+        </form>
+
+    </div>
 </body>
 </html>
