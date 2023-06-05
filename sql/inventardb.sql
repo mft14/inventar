b9155be78001a7dd02fd757b28e99df4e328032e
mft14 <metalfort14@gmail.com>
2023-06-05 11:35:36 +0200

    removed createlogins

--- sql/inventardb.sql
+++ sql/inventardb.sql
@@ -115,3 +115,27 @@ CREATE TABLE IF NOT EXISTS verantwortlichkeit
     FOREIGN KEY (inventarnummer)
     	REFERENCES inventar (inventarnummer)
 );
+
+CREATE TABLE `logins`( 
+    `pk_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(30) NOT NULL,
+    `role` VARCHAR(10) NOT NULL
+);
+
+INSERT INTO logins (username, password, role) VALUES 
+('admin', '123456', 'abt'),
+('admin', '123456', 'it1'),
+('admin', '123456', 'it2'),
+('admin', '123456', 'pers'),
+('karim', '123456', 'pers'),
+('jannik', '123456', 'it1'),
+('lukas', '123456', 'it1'),
+('lukas', '123456', 'it3'),
+('mark', '123456', 'pers'),
+('mark', '123456', 'it2'),
+('leon', '123456', 'abt'),
+('leon', '123456', 'pers'),
+('leon', '123456', 'it1');
+
+
